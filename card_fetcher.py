5d73f027eeccd080c7be309506c29ee6c0efcb88
LDanaee <kercyan@live.fr>
2018-09-15 14:24:08 +0200

    Card Fetcher expansion
    Added a get_all_from_search function that will get all cards within a set and not only the first page return by Scryfall API
    
    Expanded get_set with it
    
    Added then a function that get all cards from all standard set.
    
    STANDARD_SET_LIST contains all standard set that won't rotate in october :p

--- card_fetcher.py
+++ card_fetcher.py
@@ -2,18 +2,41 @@ import urllib.request
 import json
 import pandas as pd
 
+
+STANDARD_SET_LIST = ['m19', "dom", "rix", "xln" ]
+
+
+def get_all_card_from_search(url):
+    """During a search, will pull other cards and add them to the
+    "data" list from scryfall. Returns it if there is no other card"""
+
+    temp_dic = urllib.request.urlopen(url).read()
+    temp_dic = json.loads(temp_dic)
+    if temp_dic["has_more"]:
+        temp_dic["data"].extend(
+            get_all_card_from_search(temp_dic["next_page"]))
+        return temp_dic["data"]
+    else:
+        return temp_dic["data"]
+
 def get_set(set):
     """Search for card in a set (3 letter string)
     Return a panda dataframe with card name, price in usd, rarity and set
     """
 
-    contents = urllib.request.urlopen(
-        'https://api.scryfall.com/cards/search?q=e%3A'+set).read()
-    df = json.loads(contents)
-    df = pd.DataFrame.from_dict(df["data"])
-    df = df[['name', 'set', 'usd', 'rarity']]
+    temp_list = get_all_card_from_search('https://api.scryfall.com/cards/search?q=e:'+set)
+    df = pd.DataFrame.from_dict(temp_list)
+    df = df[['name', 'set', 'usd', 'rarity', 'id']]
     df['usd'] = df['usd'].astype('float', copy=False)
 
+    return df
+
+
+def get_all_standard():
+    """Search for all card in Standard and return a Dataframe"""
+    df = pd.DataFrame()
+    for magics_set in STANDARD_SET_LIST:
+        df = pd.concat([df, get_set(magics_set)])
     return df
 
 
